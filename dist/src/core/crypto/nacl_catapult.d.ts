4231f57e05679f2f616df72fca1b0909e69d244a
Christian Pecson <chpecson@gmail.com>
2019-09-15 21:06:16 +0700

    downgrade rxjs and rxjs-compat

--- dist/src/core/crypto/nacl_catapult.d.ts
+++ dist/src/core/crypto/nacl_catapult.d.ts
@@ -8,5 +8,5 @@ export declare const reduce: (r: any) => void;
 export declare const pack: (r: any, p: any) => void;
 export declare const scalarmult: (p: any, q: any, s: any) => void;
 export declare const scalarbase: (p: any, s: any) => void;
-export declare const unpackneg: (r: any, p: any) => 0 | -1;
-export declare const unpack: (r: any, p: any) => 0 | -1;
+export declare const unpackneg: (r: any, p: any) => -1 | 0;
+export declare const unpack: (r: any, p: any) => -1 | 0;
