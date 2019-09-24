22db5061195e9745e952573f861e7a71e2882dde
Christian Pecson <chpecson@gmail.com>
2019-09-24 15:49:16 +0700

    update minimum password to 6 characters

--- dist/src/model/wallet/Password.js
+++ dist/src/model/wallet/Password.js
@@ -20,12 +20,12 @@ Object.defineProperty(exports, "__esModule", { value: true });
  */
 class Password {
     /**
-     * Create a password with at least 8 characters
+     * Create a password with at least 6 characters
      * @param password
      */
     constructor(password) {
-        if (password.length < 8) {
-            throw new Error('Password must be at least 8 characters');
+        if (password.length < 6) {
+            throw new Error('Password must be at least 6 characters');
         }
         this.value = password;
     }
