1cc68ec1e712210c872a4e020c2566417dfb291c
Yaaccount <33345431+yaaccount@users.noreply.github.com>
2019-11-22 16:42:24 +0100

    export missing symbols from model #60

--- src/model/model.ts
+++ src/model/model.ts
@@ -20,25 +20,25 @@ export * from './Id';
 // Account
 export * from './account/Account';
 export * from './account/AccountInfo';
+export * from './account/AccountNames';
+export * from './account/AccountRestriction';
+export * from './account/AccountRestrictions';
+export * from './account/AccountRestrictionsInfo';
 export * from './account/Address';
 export * from './account/MultisigAccountGraphInfo';
 export * from './account/MultisigAccountInfo';
 export * from './account/PublicAccount';
-export * from './account/AccountRestrictions';
-export * from './account/AccountRestrictionsInfo';
-export * from './account/AccountRestriction';
 export * from './account/RestrictionModificationType';
 export * from './account/RestrictionType';
-export * from './account/AccountNames';
 
 // Blockchain
 export * from './blockchain/BlockchainScore';
 export * from './blockchain/BlockchainStorageInfo';
 export * from './blockchain/BlockInfo';
-export * from './blockchain/NetworkType';
 export * from './blockchain/MerklePathItem';
 export * from './blockchain/MerkleProofInfo';
 export * from './blockchain/MerkleProofInfoPayload';
+export * from './blockchain/NetworkType';
 
 // Config
 export * from './config/ChainConfig';
@@ -50,39 +50,40 @@ export * from './contract/Contract';
 export * from './diagnostic/ServerInfo';
 
 // Metadata
-export * from './metadata/Metadata';
-export * from './metadata/MosaicMetadata';
-export * from './metadata/NamespaceMetadata';
 export * from './metadata/AddressMetadata';
 export * from './metadata/Field';
+export * from './metadata/Metadata';
 export * from './metadata/MetadataInfo';
 export * from './metadata/MetadataType';
+export * from './metadata/MosaicMetadata';
+export * from './metadata/NamespaceMetadata';
 
 // Mosaic
 export * from './mosaic/Mosaic';
-export * from './mosaic/MosaicInfo';
 export * from './mosaic/MosaicId';
+export * from './mosaic/MosaicInfo';
+export * from './mosaic/MosaicNames';
 export * from './mosaic/MosaicNonce';
-export * from './mosaic/MosaicSupplyType';
 export * from './mosaic/MosaicProperties';
-export * from '../service/MosaicView';
-export * from '../service/MosaicAmountView';
+export * from './mosaic/MosaicPropertyType';
+export * from './mosaic/MosaicSupplyType';
 export * from './mosaic/NetworkCurrencyMosaic';
 export * from './mosaic/NetworkHarvestMosaic';
-export * from './mosaic/MosaicNames';
-export * from './mosaic/MosaicPropertyType';
+export * from '../service/MosaicView';
+export * from '../service/MosaicAmountView';
 
 // Namespace
 export * from '../service/Namespace';
-export * from './namespace/AliasType';
-export * from './namespace/Alias';
 export * from './namespace/AddressAlias';
+export * from './namespace/Alias';
+export * from './namespace/AliasActionType';
+export * from './namespace/AliasType';
+export * from './namespace/EmptyAlias';
 export * from './namespace/MosaicAlias';
 export * from './namespace/NamespaceId';
 export * from './namespace/NamespaceInfo';
 export * from './namespace/NamespaceName';
 export * from './namespace/NamespaceType';
-export * from './namespace/AliasActionType';
 
 // Node
 export * from './node/NodeInfo';
@@ -93,39 +94,43 @@ export * from './node/RoleType';
 export * from './receipt/ArtifactExpiryReceipt';
 export * from './receipt/BalanceChangeReceipt';
 export * from './receipt/BalanceTransferReceipt';
+export * from './receipt/InflationReceipt';
 export * from './receipt/Receipt';
 export * from './receipt/ReceiptSource';
 export * from './receipt/ReceiptType';
 export * from './receipt/ReceiptVersion';
 export * from './receipt/ResolutionEntry';
 export * from './receipt/ResolutionStatement';
-export * from './receipt/TransactionStatement';
 export * from './receipt/ResolutionType';
-export * from './receipt/InflationReceipt';
 export * from './receipt/Statement';
+export * from './receipt/TransactionStatement';
 
 // Transaction
-export * from './transaction/AccountLinkTransaction';
 export * from './transaction/AccountAddressRestrictionModificationTransaction';
-export * from './transaction/AccountOperationRestrictionModificationTransaction';
+export * from './transaction/AccountLinkTransaction';
 export * from './transaction/AccountMosaicRestrictionModificationTransaction';
+export * from './transaction/AccountOperationRestrictionModificationTransaction';
 export * from './transaction/AccountRestrictionModification';
+export * from './transaction/AccountRestrictionTransaction';
 export * from './transaction/AddressAliasTransaction';
 export * from './transaction/AggregateTransaction';
 export * from './transaction/AggregateTransactionCosignature';
 export * from './transaction/AggregateTransactionInfo';
+export * from './transaction/AliasTransaction';
 export * from './transaction/ChainConfigTransaction';
 export * from './transaction/ChainUpgradeTransaction';
 export * from './transaction/CosignatureSignedTransaction';
 export * from './transaction/CosignatureTransaction';
 export * from './transaction/Deadline';
 export * from './transaction/EncryptedMessage';
+export * from './transaction/FeeCalculationStrategy';
 export * from './transaction/HashLockTransaction';
 export * from './transaction/HashType';
 export * from './transaction/InnerTransaction';
 export * from './transaction/LinkAction';
 export * from './transaction/LockFundsTransaction';
 export * from './transaction/Message';
+export * from './transaction/MessageType';
 export * from './transaction/ModifyContractTransaction';
 export * from './transaction/ModifyMetadataTransaction';
 export * from './transaction/ModifyMultisigAccountTransaction';
@@ -141,12 +146,13 @@ export * from './transaction/SecretProofTransaction';
 export * from './transaction/SignedTransaction';
 export * from './transaction/SyncAnnounce';
 export * from './transaction/Transaction';
-export * from './transaction/TransactionBuilderFactory';
 export * from './transaction/TransactionAnnounceResponse';
+export * from './transaction/TransactionBuilderFactory';
 export * from './transaction/TransactionInfo';
 export * from './transaction/TransactionStatus';
 export * from './transaction/TransactionStatusError';
 export * from './transaction/TransactionType';
+export * from './transaction/TransactionVersion';
 export * from './transaction/TransferTransaction';
 
 // Upgrade
