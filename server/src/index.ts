12818d8a662654921f41584f9f34d37b7236fbce
Yong Ming-Jia <yongmingjia0112@gmail.com>
2021-05-03 15:41:11 +0800

    Add some production code but been commented and add the route to the main file

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,14 +1,30 @@
 import express from 'express';
 import cors from 'cors';
+// import path from 'path';
+// import enforce from 'express-sslify';
+
+// Route
+import downloadPrivateKeyRoute from './Route/DownloadPrivateKey';
 
 const app = express();
 
 if (process.env.NODE_ENV !== 'production') require('dotenv').config();
 
+// if (process.env.NODE_ENV === 'production') {
+//   app.use(enforce.HTTPS({ trustProtoHeader: true }));
+//   app.use(express.static(path.join(__dirname, 'client/build')));
+
+//   app.get('*', function (req, res) {
+//     res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+//   });
+// }
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(cors());
+
+app.use(downloadPrivateKeyRoute);
 
 app.listen(process.env.SERVER_PORT, () => {
   console.log('Server port number:', process.env.SERVER_PORT);
