b456ae5726194097c9f02c3dbc35c84bd479d0b6
Jason-Goay <jasongoay1315@gmail.com>
2021-05-04 22:42:14 +0800

    backend Code

--- server/src/Route/SignInAndUp.ts
+++ server/src/Route/SignInAndUp.ts
@@ -3,13 +3,20 @@
 import express from 'express';
 
 const router = express.Router();
+const port = process.env.PORT || 5000;
 
-router.get('/sign-in', (req, res, next) => {});
+
+router.post('/sign-in', (req, res, next) => {
+  res.send("<h1>Hello</h1>")
+});
 
 router.post('/sign-up', (req, res, next) => {
+
   // TODO - Handle Sign up
   // TODO - Add the Create Xpx Acc at the end
   // TODO - Store to firebase
 });
+
+
 
 export default router;
