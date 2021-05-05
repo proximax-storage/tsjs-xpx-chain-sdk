63f49a671bc6146e4c1e26ddb53cb3397f34827e
Yong Ming-Jia <yongmingjia0112@gmail.com>
2021-05-05 21:42:45 +0800

    Minor update for button and adding required dependencies to package.json

--- client/src/Page/SignUp.tsx
+++ client/src/Page/SignUp.tsx
@@ -34,22 +34,22 @@ const SignUp: React.FC = () => {
 
   return (
     // {...register())} registers inputs to be validated
-    <form onSubmit={onSubmit}>
+    <form onSubmit={onSubmit} className="sign-up-form">
       <div>
-        <label htmlFor='username'>Username</label>
+        <label htmlFor="username">Username</label>
         <input
           {...register('username', { required: 'Required' })}
-          name='username'
-          placeholder='Username'
-          type='text'
+          name="username"
+          placeholder="Username"
+          type="text"
         />
         {errors.username && (
-          <div className='error'>{errors.username.message}</div>
+          <div className="error">{errors.username.message}</div>
         )}
       </div>
 
       <div>
-        <label htmlFor='emailAddress'>Email Address</label>
+        <label htmlFor="emailAddress">Email Address</label>
         <input
           {...register('emailAddress', {
             required: 'Required',
@@ -58,19 +58,19 @@ const SignUp: React.FC = () => {
               message: 'invalid email address',
             },
           })}
-          name='emailAddress'
-          type='emailAddress'
-          placeholder='Email Address'
+          name="emailAddress"
+          type="emailAddress"
+          placeholder="Email Address"
         />
         {errors.emailAddress && (
-          <div className='error'>{errors.emailAddress.message}</div>
+          <div className="error">{errors.emailAddress.message}</div>
         )}
       </div>
 
       <br />
 
       <div>
-        <label htmlFor='password'>Password</label>
+        <label htmlFor="password">Password</label>
         <input
           {...register('password', {
             required: 'Required',
@@ -83,35 +83,38 @@ const SignUp: React.FC = () => {
               message: 'Password should have 8 to 30 characters',
             },
           })}
-          name='password'
-          type='password'
-          placeholder='Password (8 - 30 characters)'
+          name="password"
+          type="password"
+          placeholder="Password (8 - 30 characters)"
           onChange={onChange}
         />
         {errors.password && (
-          <div className='error'>{errors.password.message}</div>
+          <div className="error">{errors.password.message}</div>
         )}
       </div>
 
       <div>
-        <label htmlFor='confirmPassword'>Confirm Password</label>
+        <label htmlFor="confirmPassword">Confirm Password</label>
         <input
           {...register('confirmPassword', {
             required: 'Required',
             validate: (v) => v === password || 'Password does not match',
           })}
-          name='confirmPassword'
-          type='password'
-          placeholder='Re-enter Password'
+          name="confirmPassword"
+          type="password"
+          placeholder="Re-enter Password"
         />
         {errors.confirmPassword && (
-          <div className='error'>{errors.confirmPassword.message}</div>
+          <div className="error">{errors.confirmPassword.message}</div>
         )}
       </div>
 
       <br />
 
-      <button type='submit'>Sign Up</button>
+      {/* Styling for submit button */}
+      <button type="submit" className={`valid-button`}>
+        Sign Up
+      </button>
     </form>
   );
 };
