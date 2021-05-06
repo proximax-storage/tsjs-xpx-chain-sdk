aa011a97cd1b250a8e876ed4785ec52c263dc0dd
Yong Ming-Jia <yongmingjia0112@gmail.com>
2021-05-06 14:56:35 +0800

    Update server

--- client/src/Page/SignUp.tsx
+++ client/src/Page/SignUp.tsx
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import React, { useState } from 'react';
 import { useForm } from 'react-hook-form';
 import './SignUp.scss';
@@ -21,35 +22,41 @@ const SignUp: React.FC = () => {
     formState: { errors },
   } = useForm<User>();
 
-  // Dummy onSubmit function
-  const onSubmit = handleSubmit((data) => {
-    alert(JSON.stringify(data));
+  // Route to server
+  const onSubmit = handleSubmit(async (data) => {
+    const { emailAddress, password } = data;
+
+    const res = await axios.post('/api/sign-up', {
+      email: emailAddress,
+      password: password,
+    });
+
+    console.log(res);
   });
 
   // Update password state onChange
   const onChange = (e: any) => {
     setPassword(e.target.value);
-    console.log(password);
   };
 
   return (
     // {...register())} registers inputs to be validated
-    <form onSubmit={onSubmit} className="sign-up-form">
+    <form onSubmit={onSubmit} className='sign-up-form'>
       <div>
-        <label htmlFor="username">Username</label>
+        <label htmlFor='username'>Username</label>
         <input
           {...register('username', { required: 'Required' })}
-          name="username"
-          placeholder="Username"
-          type="text"
+          name='username'
+          placeholder='Username'
+          type='text'
         />
         {errors.username && (
-          <div className="error">{errors.username.message}</div>
+          <div className='error'>{errors.username.message}</div>
         )}
       </div>
 
       <div>
-        <label htmlFor="emailAddress">Email Address</label>
+        <label htmlFor='emailAddress'>Email Address</label>
         <input
           {...register('emailAddress', {
             required: 'Required',
@@ -58,19 +65,19 @@ const SignUp: React.FC = () => {
               message: 'invalid email address',
             },
           })}
-          name="emailAddress"
-          type="emailAddress"
-          placeholder="Email Address"
+          name='emailAddress'
+          type='emailAddress'
+          placeholder='Email Address'
         />
         {errors.emailAddress && (
-          <div className="error">{errors.emailAddress.message}</div>
+          <div className='error'>{errors.emailAddress.message}</div>
         )}
       </div>
 
       <br />
 
       <div>
-        <label htmlFor="password">Password</label>
+        <label htmlFor='password'>Password</label>
         <input
           {...register('password', {
             required: 'Required',
@@ -83,36 +90,36 @@ const SignUp: React.FC = () => {
               message: 'Password should have 8 to 30 characters',
             },
           })}
-          name="password"
-          type="password"
-          placeholder="Password (8 - 30 characters)"
+          name='password'
+          type='password'
+          placeholder='Password (8 - 30 characters)'
           onChange={onChange}
         />
         {errors.password && (
-          <div className="error">{errors.password.message}</div>
+          <div className='error'>{errors.password.message}</div>
         )}
       </div>
 
       <div>
-        <label htmlFor="confirmPassword">Confirm Password</label>
+        <label htmlFor='confirmPassword'>Confirm Password</label>
         <input
           {...register('confirmPassword', {
             required: 'Required',
             validate: (v) => v === password || 'Password does not match',
           })}
-          name="confirmPassword"
-          type="password"
-          placeholder="Re-enter Password"
+          name='confirmPassword'
+          type='password'
+          placeholder='Re-enter Password'
         />
         {errors.confirmPassword && (
-          <div className="error">{errors.confirmPassword.message}</div>
+          <div className='error'>{errors.confirmPassword.message}</div>
         )}
       </div>
 
       <br />
 
       {/* Styling for submit button */}
-      <button type="submit" className={`valid-button`}>
+      <button type='submit' className={`valid-button`}>
         Sign Up
       </button>
     </form>
