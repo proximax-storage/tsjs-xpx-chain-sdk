d825b5a467d1535cd2eff0273a2b12b882afa009
Yong Ming-Jia <yongmingjia0112@gmail.com>
2021-05-12 20:19:53 +0800

    Lock down sign-up-success url, prevent any user to access the link by themselves

--- client/src/Component/Routing.tsx
+++ client/src/Component/Routing.tsx
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Switch, Route } from 'react-router-dom';
 import PrivateRoute from './PrivateRoute';
+
+import { useAuth } from '../Context/AuthContext';
 
 import SignUp from '../Page/SignUp';
 import SignIn from '../Page/SignIn';
@@ -8,12 +10,16 @@ import Home from '../Page/Home';
 import SignUpSuccess from '../Page/SignUpSuccess';
 
 const Routing: React.FC = () => {
+  const { hasXpxAcc } = useAuth();
+
   return (
     <Switch>
-      <PrivateRoute path='/sign-up-success' component={SignUpSuccess} />
+      {!hasXpxAcc && (
+        <PrivateRoute path='/sign-up-success' component={SignUpSuccess} />
+      )}
       <Route path='/sign-up' component={SignUp} />
       <Route path='/sign-in' component={SignIn} />
-      {/* <Route path='/faq' component={SignUpSuccess} /> */}
+      <Route path='/faq' component={SignUpSuccess} />
       <PrivateRoute path='/' component={Home} />
     </Switch>
   );
