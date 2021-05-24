c95b26cfd82cd923057d6ef0414abcb5ab2f2409
Yong Ming-Jia <yongmingjia0112@gmail.com>
2021-05-24 16:37:26 +0800

    Fix CORS issue

--- client/src/Util/API/NavBarHomeAPI.ts
+++ client/src/Util/API/NavBarHomeAPI.ts
@@ -2,9 +2,13 @@ import axios from 'axios';
 import API_URL from './EndPoint';
 
 const getUsernameByUid = async (uid: string) => {
-  return await axios.post(`${API_URL}/api/get-username`, {
-    uid: uid,
-  });
+  return await axios.post(
+    `${API_URL}/api/get-username`,
+    {
+      uid: uid,
+    },
+    { headers: { 'Access-Control-Allow-Origin': '*' } }
+  );
 };
 
 export { getUsernameByUid };
--- client/src/Util/API/SignUpAPI.ts
+++ client/src/Util/API/SignUpAPI.ts
@@ -2,17 +2,25 @@ import axios from 'axios';
 import API_URL from './EndPoint';
 
 const postCreateAcc = async (uid: string, email: string, username: string) => {
-  await axios.post(`${API_URL}/api/create-acc`, {
-    uid: uid,
-    email: email,
-    username: username,
-  });
+  await axios.post(
+    `${API_URL}/api/create-acc`,
+    {
+      uid: uid,
+      email: email,
+      username: username,
+    },
+    { headers: { 'Access-Control-Allow-Origin': '*' } }
+  );
 };
 
 const downloadPrivateKey = async (uid: string) => {
-  const res = await axios.post(`${API_URL}/api/download-private-key`, {
-    uid: uid,
-  });
+  const res = await axios.post(
+    `${API_URL}/api/download-private-key`,
+    {
+      uid: uid,
+    },
+    { headers: { 'Access-Control-Allow-Origin': '*' } }
+  );
 
   return res;
 };
