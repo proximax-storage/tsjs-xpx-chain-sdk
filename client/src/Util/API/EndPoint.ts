fe76380d8f4044bfc8dcc1bbbcbfd17e6f29c104
Yong Ming-Jia <yongmingjia0112@gmail.com>
2021-05-30 18:35:57 +0800

    Adding option to contact with deployed server

--- client/src/Util/API/EndPoint.ts
+++ client/src/Util/API/EndPoint.ts
@@ -1,6 +1,8 @@
-const API_URL =
-  process.env.NODE_ENV === 'development'
-    ? 'http://localhost:5000'
-    : 'https://mass-check-server.herokuapp.com';
+// const API_URL =
+//   process.env.NODE_ENV === 'development'
+//     ? 'http://localhost:5000'
+//     : 'https://mass-check-server.herokuapp.com';
+
+const API_URL = 'https://mass-check-server.herokuapp.com';
 
 export default API_URL;
