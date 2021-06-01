f958c15e93b93555ec25e3acb9b7f8343134f510
Yong Ming-Jia <yongmingjia0112@gmail.com>
2021-06-02 00:06:40 +0800

    Refactor Step Two Template

--- client/src/Page/Investigate/InvestigateStepTwo.tsx
+++ client/src/Page/Investigate/InvestigateStepTwo.tsx
@@ -1,37 +1,8 @@
 import React from 'react';
-import { useHistory } from 'react-router-dom';
-import { LocalStorageEnum } from '../../Util/Constant/LocalStorageEnum';
-
-import './InvestigateStepTwo.scss';
+import StepTwo from '../Template/StepTwo';
 
 const InvestigateStepTwo: React.FC = () => {
-  const history = useHistory();
-
-  const onCancel = () => {
-    history.push('/');
-  };
-
-  // TODO Match tweet
-  const matchTweet = () => {
-    localStorage.setItem(LocalStorageEnum.IS_FIRST_COUNTDOWN, 'true');
-    history.push('/investigate-step-three');
-  };
-
-  return (
-    <div className='investigate-step-two'>
-      <div className='investigate-step-two__animation'></div>
-      <div className='investigate-step-two__description'>
-        <br />
-        <p>Matching you to a news tweet...</p>
-      </div>
-      <button className='investigate-step-two__button' onClick={onCancel}>
-        Cancel
-      </button>
-      <button className='investigate-step-two__button' onClick={matchTweet}>
-        Next
-      </button>
-    </div>
-  );
+  return <StepTwo nextUrl='/investigate-step-two' />;
 };
 
 export default InvestigateStepTwo;
---
+++ client/src/Page/Template/StepTwo.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { useHistory } from 'react-router-dom';
+import { LocalStorageEnum } from '../../Util/Constant/LocalStorageEnum';
+
+import './StepTwo.scss';
+
+const InvestigateStepTwo = ({ nextUrl }) => {
+  const history = useHistory();
+
+  const onCancel = () => {
+    history.push('/');
+  };
+
+  // TODO Match tweet
+  const matchTweet = () => {
+    localStorage.setItem(LocalStorageEnum.IS_FIRST_COUNTDOWN, 'true');
+    history.push(nextUrl);
+  };
+
+  return (
+    <div className='step-two'>
+      <div className='step-two__animation'></div>
+      <div className='step-two__description'>
+        <br />
+        <p>Matching you to a news tweet...</p>
+      </div>
+      <button className='step-two__button' onClick={onCancel}>
+        Cancel
+      </button>
+      <button className='step-two__button' onClick={matchTweet}>
+        Next
+      </button>
+    </div>
+  );
+};
+
+export default InvestigateStepTwo;
