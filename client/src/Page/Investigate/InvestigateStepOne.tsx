1588b5896776191f403a4935df12b182460da55e
Yong Ming-Jia <yongmingjia0112@gmail.com>
2021-06-01 22:30:02 +0800

    Fix typo

--- client/src/Page/Investigate/InvestigateStepOne.tsx
+++ client/src/Page/Investigate/InvestigateStepOne.tsx
@@ -7,14 +7,14 @@ const InvestigateStepOne: React.FC = () => {
   const onNextPage = () => {
     history.push('/investigate-step-two');
   };
-  
+
   return (
     <div className='investigate-step-one'>
       <div className='investigate-step-one__image'></div>
       <div className='investigate-step-one_description'>
         <p>
           Want to unearth the truth? Contribute to our cause and detect fake
-          news by being a <strong>Juror</strong>.
+          news by being a <strong>Investigator</strong>.
         </p>
         <br />
         <p>Click on the button to be queued for a news tweet!</p>
