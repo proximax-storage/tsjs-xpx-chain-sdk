7e4b9a451133180a62f8a2ce7f73fe5c4ed03df9
Chun Lam <chunlam.sees@gmail.com>
2021-10-06 15:29:18 +0800

    use substring instead of substr

--- src/model/transaction/Message.ts
+++ src/model/transaction/Message.ts
@@ -29,7 +29,7 @@ export abstract class Message {
     public static decodeHex(hex: string): string {
         let str = '';
         for (let i = 0; i < hex.length; i += 2) {
-            str += String.fromCharCode(parseInt(hex.substr(i, 2), 16));
+            str += String.fromCharCode(parseInt(hex.substring(i, i + 2), 16));
         }
         try {
             return decode(str);
