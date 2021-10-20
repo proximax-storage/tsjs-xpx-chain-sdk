1432325b8ca7137f08ce9ce7afbe4e26664368aa
Chun Lam <chunlam.sees@gmail.com>
2021-10-20 10:47:58 +0800

    update deadline model

--- src/model/transaction/Deadline.ts
+++ src/model/transaction/Deadline.ts
@@ -31,7 +31,12 @@ export class Deadline {
     /**
      * Deadline value
      */
-    public value: LocalDateTime;
+    public value: LocalDateTime | null;
+
+    /**
+     * Adjusted value
+     */
+    public adjustedValue: UInt64;
 
     /**
      * Create deadline model
@@ -50,8 +55,12 @@ export class Deadline {
             throw new Error('deadline should be less than 24 hours');
         }
 
-        return new Deadline(deadlineDateTime);
-    }
+        const adjustedValue = UInt64.fromUint(
+            (deadlineDateTime.atZone(ZoneId.SYSTEM).toInstant().toEpochMilli() - Deadline.timestampNemesisBlock * 1000),
+        );
+
+        return new Deadline(deadlineDateTime, adjustedValue);
+    } 
 
     /**
      * @internal
@@ -59,27 +68,39 @@ export class Deadline {
      * @returns {Deadline}
      */
     public static createFromDTO(value: number[]): Deadline {
-        const dateSeconds = (new UInt64(value)).compact();
+        const adjustedValue = new UInt64(value);
+        const dateSeconds = adjustedValue.compact();
         const deadline = LocalDateTime.ofInstant(
             Instant.ofEpochMilli(Math.round(dateSeconds + Deadline.timestampNemesisBlock * 1000)),
             ZoneId.SYSTEM);
-        return new Deadline(deadline);
+        return new Deadline(deadline, adjustedValue);
     }
 
     /**
      * @param deadline
      */
-    private constructor(deadline: LocalDateTime) {
+    private constructor(deadline: LocalDateTime | null, adjustedValue: UInt64) {
         this.value = deadline;
+        this.adjustedValue = adjustedValue;
+    }
+
+    /**
+     * @internal
+     * @param value
+     * @returns {Deadline}
+     */
+     public static createEmpty(): Deadline {
+        return new Deadline(null, UInt64.fromUint(0));
     }
 
     /**
      * @internal
      */
     public toDTO(): number[] {
-        return UInt64.fromUint(
-            (this.value.atZone(ZoneId.SYSTEM).toInstant().toEpochMilli() - Deadline.timestampNemesisBlock * 1000),
-        ).toDTO();
+        return this.adjustedValue.toDTO();
+        // return UInt64.fromUint(
+        //      (this.value.atZone(ZoneId.SYSTEM).toInstant().toEpochMilli() - Deadline.timestampNemesisBlock * 1000),
+        // ).toDTO();
     }
 }
 
