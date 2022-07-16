eb4afd22c8bf8c798d94d34e5b373ef3804e9029
Chun Lam <chunlam.sees@gmail.com>
2022-07-16 13:51:00 +0800

    add in DTO type map

--- src/infrastructure/model/models.ts
+++ src/infrastructure/model/models.ts
@@ -224,6 +224,7 @@ export * from './verifiableEntityDTO';
 
 import { AccountDTO } from './accountDTO';
 import { AccountExchangeDTO } from './accountExchangeDTO';
+import { AccountSdaExchangeDTO } from './accountSdaExchangeDTO';
 import { AccountIds } from './accountIds';
 import { AccountInfoDTO } from './accountInfoDTO';
 import { AccountLinkTransactionBodyDTO } from './accountLinkTransactionBodyDTO';
@@ -308,6 +309,8 @@ import { EntityTypeEnum } from './entityTypeEnum';
 import { ExchangeOfferTransactionBodyDTO } from './exchangeOfferTransactionBodyDTO';
 import { ExchangeOfferTransactionDTO } from './exchangeOfferTransactionDTO';
 import { ExchangesDTO } from './exchangesDTO';
+import { SdaOfferInfoDTO } from './sdaOfferInfoDTO';
+import { SdaOfferInfoWithOwnerDTO } from './sdaOfferInfoWithOwnerDTO';
 import { FailureVerificationDTO } from './failureVerificationDTO';
 import { FieldDTO } from './fieldDTO';
 import { FileDTO } from './fileDTO';
@@ -472,6 +475,7 @@ let enumsMap: {[index: string]: any} = {
 let typeMap: {[index: string]: any} = {
     "AccountDTO": AccountDTO,
     "AccountExchangeDTO": AccountExchangeDTO,
+    "AccountSdaExchangeDTO": AccountSdaExchangeDTO,
     "AccountIds": AccountIds,
     "AccountInfoDTO": AccountInfoDTO,
     "AccountLinkTransactionBodyDTO": AccountLinkTransactionBodyDTO,
@@ -550,6 +554,8 @@ let typeMap: {[index: string]: any} = {
     "ExchangeOfferTransactionBodyDTO": ExchangeOfferTransactionBodyDTO,
     "ExchangeOfferTransactionDTO": ExchangeOfferTransactionDTO,
     "ExchangesDTO": ExchangesDTO,
+    "SdaOfferInfoDTO": SdaOfferInfoDTO,
+    "SdaOfferInfoWithOwnerDTO": SdaOfferInfoWithOwnerDTO,
     "FailureVerificationDTO": FailureVerificationDTO,
     "FieldDTO": FieldDTO,
     "FileDTO": FileDTO,
